70479ac5ead50feefb5699b15fc49f857705ef81
POWER_OF_RAGNAROS <persukhov_s@mail.ru>
2016-04-09 10:39:26 +0200

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,2 @@
 print ("hello world!")
-print ("hello world!")
-print ("hello world!")
-print('hello everyone!')
-print('hello all!')
-print(((((("Don't touch this file!" + "!!!")))))) #(and this string!!!)
-print("Hello everybody")
+
