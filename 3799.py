f6697a2c61601a54b051c449eef5b0129b9618ca
gurovic <gurovic@gmail.com>
2016-04-09 12:29:30 +0300

    Убрали восклицательные знаки

--- 3799.py
+++ 3799.py
@@ -1,2 +1,2 @@
-#http://informatics.mccme.ru/mod/statements/view3.php?id=3962&chapterid=3799#1
-#РЕКУРСИЯ!!!
+﻿#http://informatics.mccme.ru/mod/statements/view3.php?id=3962&chapterid=3799#1
+#РЕКУРСИЯ!
