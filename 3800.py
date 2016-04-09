6655f044baab6cbb8b8153af35e5f987420e9c69
mex3 <3meandme@gmail.com>
2016-04-09 16:56:39 +0300

    Create 3800.py

---
+++ 3800.py
@@ -0,0 +1,10 @@
+#http://informatics.mccme.ru/mod/statements/view3.php?id=3962&chapterid=3800#1
+#Рекурсия! Должна быть рекурсия!
+
+def power(a,n):
+  a = a^n
+  print(a)
+
+a=input()
+b=input()
+power(a,b)
