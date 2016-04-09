acd4087c6547bf6d30ab396456a081682724cd3d
MatveevAlexy <matveev2001a@gmail.com>
2016-04-09 12:43:54 +0300

    Update 197.py

--- 197.py
+++ 197.py
@@ -1,1 +1,19 @@
 #http://informatics.mccme.ru/mod/statements/view.php?id=197#1
+z = x = input()
+i = 1
+w = 0
+while w == 0:
+    o = str(i)
+    q = 0
+    x = z
+    while q < len(o) and w == 0:
+        if x.find(o[q]) != -1:
+            p = x.find(o[q]) + 1
+            x = x[p:]
+            q += 1
+        else:
+            w = 1
+        i += 1
+m = i - 1
+print(m)
+# вот программа, но только не очень верная
