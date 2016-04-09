926c2c8474fc7d68d96111ca6e2fb454d7b58208
mex3 <3meandme@gmail.com>
2016-04-09 12:45:51 +0300

    Update 424.py
    Добавлена заготовка под вывод в файл

--- 424.py
+++ 424.py
@@ -7,3 +7,11 @@ DayOfTheWeek = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturda
 MonthNums = {'January': 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5, 'June': 6, 'July': 7, 'August': 8, 'September': 9, 'October': 10, 'November': 11, 'December': 12}
 DayNums = {'Monday': 1, 'Tuesday': 2, 'Wednesday': 3, 'Thursday': 4, 'Friday': 5, 'Saturday': 6, 'Sunday': 7}
 Months = (0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+
+fout = open("output.txt", "w")
+# days = ((0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31), (0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31))
+day_start, l, k = map(int, fin.readline().split())
+column = day_start
+weeks_month = [1] * 13
+
+fout.close()
