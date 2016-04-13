29d9cf30a9f2684bbda2fd394113c307bcf22f4f
russiandeveloper17 <bugamishka@gmail.com>
2016-04-13 20:18:51 +0300

    Update 3800.py

--- 3800.py
+++ 3800.py
@@ -1,10 +1,18 @@
 #http://informatics.mccme.ru/mod/statements/view3.php?id=3962&chapterid=3800#1
 #Рекурсия! Должна быть рекурсия!
 
-def power(a,n):
-  a = a^n
-  print(a)
 
-a=input()
-b=input()
-power(a,b)
+def power(a, n):
+    if n == 0:
+        return 1
+    return(a * power(a, n - 1))
+a = input()
+try:
+    a = int(a)
+except:
+    a = float(a)
+    n = int(input())
+    print(power(a, n))
+else:
+    n = int(input())
+    print(power(a, n))
