ebea8b4aa61943022a7a6260ec5000a534100ca2
russiandeveloper17 <bugamishka@gmail.com>
2016-04-13 20:15:55 +0300

    Update 3799.py

--- 3799.py
+++ 3799.py
@@ -1,3 +1,17 @@
 ﻿#http://informatics.mccme.ru/mod/statements/view3.php?id=3962&chapterid=3799#1
 #РЕКУРСИЯ!
 #РЕКУРСИЯ!
+
+#this code works
+#and I don't know why we have to use recursion
+
+def IsPrime(n):
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+if IsPrime(int(input())):
+    print('YES')
+else:
+    print('NO')
