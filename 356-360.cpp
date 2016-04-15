0ba55d56237f4dcda7ae514a1f9b96af584921a8
russiandeveloper17 <bugamishka@gmail.com>
2016-04-15 21:17:32 +0300

    Update 356-360.cpp

--- 356-360.cpp
+++ 356-360.cpp
@@ -9,3 +9,16 @@ for i in range(n):
         mx = x
         mn = i
 print(mx, mn)
+
+#B:
+n, m = map(int, input().split())
+mr, mn, mm = 0, 0, 0
+for i in range(n):
+    s = list(map(int, input().split()))
+    for x in range(m):
+        if s[x] > mr:
+            mr = s[x]
+            mn = i
+            mm = x
+print(mr)
+print(mn, mm)
