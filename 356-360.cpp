58d44ac9527c6a261da2eba4b03824964309fb19
russiandeveloper17 <bugamishka@gmail.com>
2016-04-15 20:56:46 +0300

    Update 356-360.cpp

--- 356-360.cpp
+++ 356-360.cpp
@@ -1,2 +1,11 @@
 #http://informatics.mccme.ru/mod/statements/view.php?id=15232#1
 #решить несколько задач из 5
+#A:
+n, m = map(int, input().split())
+mx, mn = 0, n
+for i in range(n):
+    x = sum(map(int, input().split()))
+    if x > mx:
+        mx = x
+        mn = i
+print(mx, mn)
