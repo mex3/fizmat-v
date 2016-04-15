11afdedaaa05b65651c46fcb3c332e8d82ac9a21
Mihail Bugaev <bugamishka@gmail.com>
2016-04-15 22:06:15 +0300

    There're three codes

--- 356-360.cpp
+++ 356-360.cpp
@@ -22,3 +22,23 @@ for i in range(n):
             mm = x
 print(mr)
 print(mn, mm)
+
+#C:
+n, m = map(int, input().split())
+mr, mn, mm, ms = 0, 0, 0, 0
+for i in range(n):
+    s = list(map(int, input().split()))
+    for x in range(m):
+        if s[x] == mr:
+            if sum(s) > ms:
+                mr = s[x]
+                mn = i
+                mm = x
+                ms = sum(s)
+        if s[x] > mr:
+            mr = s[x]
+            mn = i
+            mm = x
+            ms = sum(s)
+print(mn)
+#3 из 5 - это ведь несколько, да?
