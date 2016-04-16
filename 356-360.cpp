d388ca25074abac9d1b005e22687176f427feef0
marytomilina <tomilina.mary2001@gmail.com>
2016-04-16 12:42:39 +0300

    Update 356-360.cpp

--- 356-360.cpp
+++ 356-360.cpp
@@ -48,3 +48,8 @@ for i in range(n):
     l.append(sum(map(int, input().split())))
 print(l.count(max(l)))
 #this code doesn't work right, and I don't know why
+#marytomilina #A
+n, m = map(int, input().split())
+A = [sum(map(int, input().split())) for z in range(n)]
+print(max(A))
+print(A.index(max(A)))
