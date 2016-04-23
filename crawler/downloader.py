fc378d53fa6f94f14f966e8595070e70918b572f
gurovic <gurovic@gmail.com>
2016-04-23 11:44:18 +0300

    Crawler added

---
+++ crawler/downloader.py
@@ -0,0 +1,4 @@
+#Читает ссылку из файла linklist.txt
+#Скачивает документ по ссылке в папку downloaded
+#http://stackoverflow.com/questions/450285/executing-command-line-programs-from-within-python
+#На выбор: http://stackoverflow.com/questions/450285/executing-command-line-programs-from-within-python
---
+++ crawler/parse-for-links.py
@@ -0,0 +1,6 @@
+#анализирует файлы в папке downloaded
+#извлекает ссылки в рамках этого же домена
+#сохраняет их в файл linksraw.txt
+#Как работать с регулярными выражениями: https://docs.python.org/2/howto/regex.html
+#Можно использовать готовый html-парсер, если он понятнее: https://docs.python.org/2/library/htmlparser.html
+#Концепция регулярных выражений: https://ru.wikipedia.org/wiki/%D0%A0%D0%B5%D0%B3%D1%83%D0%BB%D1%8F%D1%80%D0%BD%D1%8B%D0%B5_%D0%B2%D1%8B%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D1%8F
