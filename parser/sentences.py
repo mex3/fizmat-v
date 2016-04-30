49489c4e9c03761d7e9346f64841f60ac447fc35
Mihail Bugaev <bugamishka@gmail.com>
2016-04-30 12:20:30 +0300

    Developing sentences.py

--- parser/sentences.py
+++ parser/sentences.py
@@ -5,11 +5,11 @@ from os import listdir #this is a magical function that makes a list of files in
 res = []
 for filename in listdir(paragraphs):
     with open(paragraphs + '/' + filename) as f:
-        for s0 in f.split('...'):
-            for s1 in s0.split('!'):
-                for s2 in s1.split('?'):
-                    for s3 in s2.split('.'):
-                        res.append(s3)
+	ns = ''
+        for s in f.read():
+			ns += s
+            if s == '.' or s == '?' or s == '!':
+                res.append(ns)
 for i in range(len(res)):
     with open('sentences/' + str(i) + '.txt', 'w') as h:
         h.write(res[i])
