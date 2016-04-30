4e748644808d0f4f1c930331a1cfd5f555b6a723
Mihail Bugaev <bugamishka@gmail.com>
2016-04-30 12:48:21 +0300

    Creating paragraphs.py

--- parser/paragraphs.py
+++ parser/paragraphs.py
@@ -1,2 +1,12 @@
 #Разбивает статьи из articles на абзацы, сохраняет результат в paragraphs
 #Убирает html-разметку
+from os import listdir
+res = []
+for filename in listdir(articles):
+	with open('articles/' + filename) as f:
+		res += list(f.read().split('\n'))
+for i in range(len(res)):
+	with open('paragraphs/' + str(i), 'w') as h:
+		h.write(res[i])
+
+		
